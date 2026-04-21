2d1477a6196e11b6bfcda54e6f7dff5113e4d21a
Marvin <marvinlim4th@gmail.com>
2026-04-20 21:56:06 -0400

    update setup 2

--- setup.sh
+++ setup.sh
@@ -1,11 +1,15 @@
 wget https://tiny.carla.org/carla-0-9-15-linux
-wget https://files.pythonhosted.org/packages/86/5a/74cf6657e5bb4b8ffe790ef92a01d7f44592a992d758c7053cd6bc75bd96/carla-0.9.15-cp310-cp310-manylinux_2_27_x86_64.whl
-tar -xvzf carla-0-9-15-linux
+tar -xvzf carla-0-9-15-linux -C CARLA_0.9.15
+
+wget -O carla-0.9.15.whl https://files.pythonhosted.org/packages/86/5a/74cf6657e5bb4b8ffe790ef92a01d7f44592a992d758c7053cd6bc75bd96/carla-0.9.15-cp310-cp310-manylinux_2_27_x86_64.whl
+
+source /opt/miniforge3/etc/profile.d/conda.sh
+
 conda create -y -n carla python=3.10
 conda activate carla
 pip install pygame numpy opencv-python pillow open3d
 pip install torch torchvision --index-url https://download.pytorch.org/whl/cu128
-pip install carla-0.9.15-cp310-cp310-manylinux_2_27_x86_64.whl
+pip install carla-0.9.15.whl
 
-wget https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64
-sudo apt install code_1.116.0-1776214182_amd64.deb
+wget -O vscode.deb https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64
+sudo apt install vscode.deb
